f2265ffbab7d9af95e3648f189055e950d6bfe1a
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-09-22 19:27:31 -0700

    initial commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kanishk Bhatia on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
