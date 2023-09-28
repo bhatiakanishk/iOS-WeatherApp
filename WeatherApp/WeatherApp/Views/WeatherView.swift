32151092e7acd3529aae71f736b133a17693f95d
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-09-28 15:56:55 -0700

    Create WeatherView.swift

---
+++ WeatherApp/WeatherApp/Views/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Kanishk Bhatia on 9/28/23.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
